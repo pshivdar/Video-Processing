f7496cc5e2522613ea30248c1b11359ec68f883d
Krazzyzhall <sshukla5@asu.edu>
2015-11-21 05:22:57 -0700

    Update get_key_frame_in_clip.m

--- get_key_frame_in_clip.m
+++ get_key_frame_in_clip.m
@@ -5,22 +5,29 @@ function [histoclip,ind]=get_key_frame_in_clip(videoclip, thresh)
 % in consequtive images if the change is more mark it as key image
 % threshhold will be between 0 and 1
 [n,~]=size(videoclip);
-din=255*255;
     tempimg=videoclip{1,1};
+    [imgm,imgn]=size(tempimg);
 temphist(:,1)=imhist(tempimg(:,:,1));
 temphist(:,2)=imhist(tempimg(:,:,2));
 temphist(:,3)=imhist(tempimg(:,:,3));
 cliphist(1,:,:)=temphist;
 changehist(1)=0;
-for i=1:n
+for i=2:n
     tempimg=videoclip{i,1};
 temphist(:,1)=imhist(tempimg(:,:,1));
 temphist(:,2)=imhist(tempimg(:,:,2));
 temphist(:,3)=imhist(tempimg(:,:,3));
 cliphist(i,:,:)=temphist;
-changehist(i)=sum(sum((temphist-cliphist(i-1,:,:))^2/din));
+din=sum(sum(sum(tempimg~=0)));
+changehist(i)=sum(sum(((cliphist(i,2:256,:)/din)-(cliphist(i-1,2:256,:)/din)).^2));
 end
 %% look for change in consequtive frames
 tind=changehist>thresh;
-ind=videoclip{tind,2};
-histoclip={cliphist,videoclip{:,2}};
+if(sum(tind)==0)
+    ind=[];
+    histoclip={cliphist,videoclip{:,2}};
+else
+    ind=videoclip{tind,2};
+    histoclip={cliphist,videoclip{:,2}};    
+end
+
